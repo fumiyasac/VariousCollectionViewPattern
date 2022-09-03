bbbfa7060c0c3794bb3f14e65a60db4055c356b8
Fumiya Sakai <just1factory@gmail.com>
2022-09-04 00:10:39 +0900

    add Project DeclarativeCollectionViewExample

---
+++ DeclarativeCollectionViewExample/DeclarativeCollectionViewExample/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  DeclarativeCollectionViewExample
+//
+//  Created by 酒井文也 on 2022/09/04.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
---
+++ DeclarativeCollectionViewExample/DeclarativeCollectionViewExample/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  DeclarativeCollectionViewExample
+//
+//  Created by 酒井文也 on 2022/09/04.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
