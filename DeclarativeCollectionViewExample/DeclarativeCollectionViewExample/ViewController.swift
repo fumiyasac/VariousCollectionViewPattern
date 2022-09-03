dcfa25c057fe724a5c0a017e2deb45347270104b
Fumiya Sakai <just1factory@gmail.com>
2022-09-04 00:26:40 +0900

    add project DeclarativeCollectionViewExample.

--- DeclarativeCollectionViewExample/DeclarativeCollectionViewExample/ViewController.swift
+++ DeclarativeCollectionViewExample/DeclarativeCollectionViewExample/ViewController.swift
@@ -5,20 +5,13 @@
 //  Created by 酒井文也 on 2022/09/04.
 //
 
-import Cocoa
+import UIKit
 
-class ViewController: NSViewController {
+class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
-    }
-
-    override var representedObject: Any? {
-        didSet {
-        // Update the view, if already loaded.
-        }
     }
 
 
