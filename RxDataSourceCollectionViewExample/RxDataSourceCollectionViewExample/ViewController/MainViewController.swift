a641732a7cf6fecfc3796fdcd2e576f3defcc34e
fumiyasac <just1factory@gmail.com>
2022-12-25 14:58:13 +0900

    RxDataSource側のプロジェクトの整理

--- RxDataSourceCollectionViewExample/RxDataSourceCollectionViewExample/ViewController/MainViewController.swift
+++ RxDataSourceCollectionViewExample/RxDataSourceCollectionViewExample/ViewController/MainViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainViewController.swift
 //  RxDataSourceCollectionViewExample
 //
 //  Created by 酒井文也 on 2022/09/04.
@@ -7,13 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class MainViewController: UIViewController {
+
+    // MARK: - Override
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
-
