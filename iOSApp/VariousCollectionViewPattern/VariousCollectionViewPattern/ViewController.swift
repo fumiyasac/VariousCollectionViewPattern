10732b64a5ad80aae167fb57f0de5d4469aa19de
fumiyasac <just1factory@gmail.com>
2023-03-18 00:24:16 +0900

    新しいUICollectionViewを利用するサンプル

---
+++ iOSApp/VariousCollectionViewPattern/VariousCollectionViewPattern/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VariousCollectionViewPattern
+//
+//  Created by 酒井文也 on 2023/03/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
