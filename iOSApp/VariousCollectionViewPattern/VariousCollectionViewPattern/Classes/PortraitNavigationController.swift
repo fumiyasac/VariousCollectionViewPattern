720c7783524a1bdc945fe2a79852e912256a5840
fumiyasac <just1factory@gmail.com>
2023-03-20 22:49:26 +0900

    別途拡張クラスの新規追加

---
+++ iOSApp/VariousCollectionViewPattern/VariousCollectionViewPattern/Classes/PortraitNavigationController.swift
@@ -0,0 +1,24 @@
+//
+//  PortraitNavigationController.swift
+//  VariousCollectionViewPattern
+//
+//  Created by 酒井文也 on 2023/03/20.
+//
+
+import Foundation
+import UIKit
+
+// MEMO: 回転を許可しない画面で適用するUINavigationController（回転を許可する画面では従来通りのUINavigationControllerを適用すれば良い）
+// ※ UITabBarControllerやUINavigationControllerが絡む回転については、ルート部分での回転に関する設定が影響する点に注意
+final class PortraitNavigationController: UINavigationController {
+
+    // MARK: - Override
+
+    override var shouldAutorotate: Bool {
+        return true
+    }
+
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        return .portrait
+    }
+}
