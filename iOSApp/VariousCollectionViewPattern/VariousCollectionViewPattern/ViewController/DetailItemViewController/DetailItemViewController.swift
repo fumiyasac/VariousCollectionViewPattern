bfe79d4bc67e012cd0209088f91b9863364584a1
fumiyasac <just1factory@gmail.com>
2023-03-21 22:41:31 +0900

    Detail画面の構築

---
+++ iOSApp/VariousCollectionViewPattern/VariousCollectionViewPattern/ViewController/DetailItemViewController/DetailItemViewController.swift
@@ -0,0 +1,17 @@
+//
+//  DetailItemViewController.swift
+//  VariousCollectionViewPattern
+//
+//  Created by 酒井文也 on 2023/03/21.
+//
+
+import UIKit
+
+final class DetailItemViewController: UIViewController {
+
+    // MARK: - Override
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
