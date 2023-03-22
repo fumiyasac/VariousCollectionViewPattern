c4ba607dccb370dd4ceb985fc1c5d4a6dcf9cb20
fumiyasac <just1factory@gmail.com>
2023-03-23 00:13:10 +0900

    Main画面用にもう一つCell要素を追加

---
+++ iOSApp/VariousCollectionViewPattern/VariousCollectionViewPattern/View/FeaturedSectionCarouselMainCollectionViewCell/FeaturedSectionCarouselMainCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  FeaturedSectionCarouselMainCollectionViewCell.swift
+//  VariousCollectionViewPattern
+//
+//  Created by 酒井文也 on 2023/03/23.
+//
+
+import UIKit
+
+final class FeaturedSectionCarouselMainCollectionViewCell: UICollectionViewCell {
+
+    // MARK: - Override
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+}
