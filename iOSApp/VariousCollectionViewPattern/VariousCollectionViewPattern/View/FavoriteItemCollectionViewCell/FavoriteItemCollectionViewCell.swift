548da47ace6affb733e2e88fcffbf0c3daecc737
fumiyasac <just1factory@gmail.com>
2023-03-24 07:25:28 +0900

    Favorite画面用にCell要素を追加

---
+++ iOSApp/VariousCollectionViewPattern/VariousCollectionViewPattern/View/FavoriteItemCollectionViewCell/FavoriteItemCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  FavoriteItemCollectionViewCell.swift
+//  VariousCollectionViewPattern
+//
+//  Created by 酒井文也 on 2023/03/24.
+//
+
+import UIKit
+
+final class FavoriteItemCollectionViewCell: UICollectionViewCell {
+
+    // MARK: - Override
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+}
