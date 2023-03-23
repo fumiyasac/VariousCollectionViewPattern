62248a1acb280f080b589e426606d26c2ea60335
fumiyasac <just1factory@gmail.com>
2023-03-24 07:27:48 +0900

    Setting画面用にCell要素を追加

---
+++ iOSApp/VariousCollectionViewPattern/VariousCollectionViewPattern/View/SettingItemCollectionViewCell/SettingItemCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  SettingItemCollectionViewCell.swift
+//  VariousCollectionViewPattern
+//
+//  Created by 酒井文也 on 2023/03/24.
+//
+
+import UIKit
+
+final class SettingItemCollectionViewCell: UICollectionViewCell {
+
+    // MARK: - Override
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+}
