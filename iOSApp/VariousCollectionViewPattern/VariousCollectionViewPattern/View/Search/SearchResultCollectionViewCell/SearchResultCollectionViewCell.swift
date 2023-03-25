b6f5313c7bf413100188fb011a53bab639499d6e
fumiyasac <just1factory@gmail.com>
2023-03-25 16:46:41 +0900

    Search画面用にCell要素を追加

---
+++ iOSApp/VariousCollectionViewPattern/VariousCollectionViewPattern/View/Search/SearchResultCollectionViewCell/SearchResultCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  SearchResultCollectionViewCell.swift
+//  VariousCollectionViewPattern
+//
+//  Created by 酒井文也 on 2023/03/25.
+//
+
+import UIKit
+
+final class SearchResultCollectionViewCell: UICollectionViewCell {
+
+    // MARK: - Override
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+}
