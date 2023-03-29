a6e48c953f123e3055321c35e0ec081b65c485fd
fumiyasac <just1factory@gmail.com>
2023-03-29 22:45:32 +0900

    Main＆DetailItem画面用のViewModel新規追加

---
+++ iOSApp/VariousCollectionViewPattern/VariousCollectionViewPattern/ViewModel/DetailItemViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  DetailItemViewModel.swift
+//  VariousCollectionViewPattern
+//
+//  Created by 酒井文也 on 2023/03/29.
+//
+
+import Combine
+import Foundation
+
+final class DetailItemViewModel {
+
+    // MARK: - Initializer
+
+    init() {
+        
+    }
+}
---
+++ iOSApp/VariousCollectionViewPattern/VariousCollectionViewPattern/ViewModel/MainViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  MainViewModel.swift
+//  VariousCollectionViewPattern
+//
+//  Created by 酒井文也 on 2023/03/29.
+//
+
+import Combine
+import Foundation
+
+final class MainViewModel {
+
+    // MARK: - Initializer
+
+    init() {
+        
+    }
+}
