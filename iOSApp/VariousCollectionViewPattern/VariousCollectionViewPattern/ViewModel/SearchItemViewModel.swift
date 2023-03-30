b4b136a810ca132a5791d8f8218a9300ce888546
fumiyasac <just1factory@gmail.com>
2023-03-30 20:33:47 +0900

    その他ViewModelの雛形を追加する

---
+++ iOSApp/VariousCollectionViewPattern/VariousCollectionViewPattern/ViewModel/SearchItemViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  SearchItemViewModel.swift
+//  VariousCollectionViewPattern
+//
+//  Created by 酒井文也 on 2023/03/30.
+//
+
+import Combine
+import Foundation
+
+final class SearchItemViewModel {
+
+    // MARK: - Initializer
+
+    init() {
+        
+    }
+}
---
+++ iOSApp/VariousCollectionViewPattern/VariousCollectionViewPattern/ViewModel/SettingViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  SettingViewModel.swift
+//  VariousCollectionViewPattern
+//
+//  Created by 酒井文也 on 2023/03/30.
+//
+
+import Combine
+import Foundation
+
+final class SettingViewModel {
+
+    // MARK: - Initializer
+
+    init() {
+        
+    }
+}
