c3fe71435c1b609301d1e3d4f8fb594edbb2c148
fumiyasac <just1factory@gmail.com>
2023-04-01 18:04:49 +0900

    ShowcaseViewの新規追加

---
+++ iOSApp/VariousCollectionViewPattern/VariousCollectionViewPattern/View/ShowCase/ShowcaseView.swift
@@ -0,0 +1,22 @@
+//
+//  ShowcaseView.swift
+//  VariousCollectionViewPattern
+//
+//  Created by 酒井文也 on 2023/04/01.
+//
+
+import SwiftUI
+
+// 1つの画面だけSwiftUIでView要素を試しに作ってみます。
+
+struct ShowcaseView: View {
+    var body: some View {
+        Text("Showcase画面: SwiftUI製で作る")
+    }
+}
+
+struct ShowcaseView_Previews: PreviewProvider {
+    static var previews: some View {
+        ShowcaseView()
+    }
+}
