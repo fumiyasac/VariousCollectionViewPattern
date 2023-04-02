6840352dfbfb5c793a47a1393cb9cb7eaa80ba78
fumiyasac <just1factory@gmail.com>
2023-04-02 23:58:45 +0900

    コメントの追記

--- iOSApp/VariousCollectionViewPattern/VariousCollectionViewPattern/View/ShowCase/ShowcaseView.swift
+++ iOSApp/VariousCollectionViewPattern/VariousCollectionViewPattern/View/ShowCase/ShowcaseView.swift
@@ -9,6 +9,9 @@ import SwiftUI
 
 // 1つの画面だけSwiftUIでView要素を試しに作ってみます。
 
+// TODO: 画面遷移用Extension作成
+// ViewModelはPropertyWrapperをベースにする
+
 struct ShowcaseView: View {
     var body: some View {
         Text("Showcase画面: SwiftUI製で作る")
