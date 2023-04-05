940a2ab5880d7fc8a028c00362c0b619215ecdb4
fumiyasac <just1factory@gmail.com>
2023-04-05 23:16:55 +0900

    ShowCaseRepositoryの雛形を新規作成

---
+++ iOSApp/VariousCollectionViewPattern/VariousCollectionViewPattern/Domain/Repository/ShowCaseRepository.swift
@@ -0,0 +1,19 @@
+//
+//  ShowCaseRepository.swift
+//  VariousCollectionViewPattern
+//
+//  Created by 酒井文也 on 2023/04/05.
+//
+
+import Foundation
+
+protocol ShowCaseRepository {}
+
+final class ShowCaseRepositoryImpl: ShowCaseRepository {
+    
+    // MARK: - Initializer
+
+    init() {
+        
+    }
+}
