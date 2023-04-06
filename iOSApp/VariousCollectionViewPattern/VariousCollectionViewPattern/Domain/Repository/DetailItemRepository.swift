323fc984d7949813ef0d33e09a7cfc7e23942996
fumiyasac <just1factory@gmail.com>
2023-04-06 23:18:13 +0900

    DetailItemRepositoryの雛形を新規作成

---
+++ iOSApp/VariousCollectionViewPattern/VariousCollectionViewPattern/Domain/Repository/DetailItemRepository.swift
@@ -0,0 +1,19 @@
+//
+//  DetailItemRepository.swift
+//  VariousCollectionViewPattern
+//
+//  Created by 酒井文也 on 2023/04/06.
+//
+
+import Foundation
+
+protocol DetailItemRepository {}
+
+final class DetailItemRepositoryImpl: DetailItemRepository {
+    
+    // MARK: - Initializer
+
+    init() {
+        
+    }
+}
