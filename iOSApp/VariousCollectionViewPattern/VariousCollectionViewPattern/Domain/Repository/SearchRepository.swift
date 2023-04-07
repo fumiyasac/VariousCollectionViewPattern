5b887f2f93ef9434accbe579d8a795093d331f1c
fumiyasac <just1factory@gmail.com>
2023-04-07 23:07:05 +0900

    SearchRepositoryの雛形を新規作成

---
+++ iOSApp/VariousCollectionViewPattern/VariousCollectionViewPattern/Domain/Repository/SearchRepository.swift
@@ -0,0 +1,19 @@
+//
+//  SearchRepository.swift
+//  VariousCollectionViewPattern
+//
+//  Created by 酒井文也 on 2023/04/07.
+//
+
+import Foundation
+
+protocol SearchRepository {}
+
+final class SearchRepositoryImpl: SearchRepository {
+    
+    // MARK: - Initializer
+
+    init() {
+        
+    }
+}
