00df224e61803392a9e0b6d0ce7567fe8bcc7072
fumiyasac <just1factory@gmail.com>
2023-04-08 18:04:52 +0900

    SettingRepositoryの雛形を新規作成

---
+++ iOSApp/VariousCollectionViewPattern/VariousCollectionViewPattern/Domain/Repository/SettingRepository.swift
@@ -0,0 +1,20 @@
+//
+//  SettingRepository.swift
+//  VariousCollectionViewPattern
+//
+//  Created by 酒井文也 on 2023/04/08.
+//
+
+import Foundation
+
+protocol SettingRepository {}
+
+final class SettingRepositoryImpl: SettingRepository {
+    
+    // MARK: - Initializer
+
+    init() {
+        
+    }
+}
+
