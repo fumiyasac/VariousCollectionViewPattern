f7f71c17b972466b2554355ceb9707d90334239b
fumiyasac <just1factory@gmail.com>
2023-04-09 22:48:48 +0900

    MainGalleryGridPaginationRepositoryの新規作成

---
+++ iOSApp/VariousCollectionViewPattern/VariousCollectionViewPattern/Domain/Repository/MainGalleryGridPaginationRepository.swift
@@ -0,0 +1,19 @@
+//
+//  MainGalleryGridPaginationRepository.swift
+//  VariousCollectionViewPattern
+//
+//  Created by 酒井文也 on 2023/04/09.
+//
+
+import Foundation
+
+protocol MainGalleryGridPaginationRepository {}
+
+final class MainGalleryGridPaginationRepositoryImpl: MainGalleryGridPaginationRepository {
+    
+    // MARK: - Initializer
+
+    init() {
+        
+    }
+}
