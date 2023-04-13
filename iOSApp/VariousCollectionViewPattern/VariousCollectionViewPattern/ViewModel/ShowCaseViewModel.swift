dfb47454e8438ea607216085d28e8272f5446f84
fumiyasac <just1factory@gmail.com>
2023-04-13 21:16:47 +0900

    ShowCaseViewModelの雛形を新規作成

---
+++ iOSApp/VariousCollectionViewPattern/VariousCollectionViewPattern/ViewModel/ShowCaseViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  ShowCaseViewModel.swift
+//  VariousCollectionViewPattern
+//
+//  Created by 酒井文也 on 2023/04/13.
+//
+
+import Combine
+import Foundation
+
+final class ShowCaseViewModel {
+
+    // MARK: - Initializer
+
+    init() {
+        
+    }
+}
