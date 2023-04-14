3a7ce4b64c339c8f2aec5c7d32d87714957b1863
fumiyasac <just1factory@gmail.com>
2023-04-14 09:09:37 +0900

    MainAnnouncementRepositoryの雛形を新規作成

---
+++ iOSApp/VariousCollectionViewPattern/VariousCollectionViewPattern/Domain/Repository/MainAnnouncementRepository.swift
@@ -0,0 +1,19 @@
+//
+//  MainAnnouncementRepository.swift
+//  VariousCollectionViewPattern
+//
+//  Created by 酒井文也 on 2023/04/14.
+//
+
+import Foundation
+
+protocol MainAnnouncementRepository {}
+
+final class MainAnnouncementRepositoryImpl: MainAnnouncementRepository {
+    
+    // MARK: - Initializer
+
+    init() {
+        
+    }
+}
