8af70bc794205d44f25a4196361bf58600d6f5a4
fumiyasac <just1factory@gmail.com>
2023-04-15 22:43:04 +0900

    MainPickupContentsRepository/MainSpecialBannerRepositoryの雛形を新規作成

---
+++ iOSApp/VariousCollectionViewPattern/VariousCollectionViewPattern/Domain/Repository/MainPickupContentsRepository.swift
@@ -0,0 +1,19 @@
+//
+//  MainPickupContentsRepository.swift
+//  VariousCollectionViewPattern
+//
+//  Created by 酒井文也 on 2023/04/15.
+//
+
+import Foundation
+
+protocol MainPickupContentsRepository {}
+
+final class MainPickupContentsRepositoryImpl: MainPickupContentsRepository {
+
+    // MARK: - Initializer
+
+    init() {
+        
+    }
+}
---
+++ iOSApp/VariousCollectionViewPattern/VariousCollectionViewPattern/Domain/Repository/MainSpecialBannerRepository.swift
@@ -0,0 +1,19 @@
+//
+//  MainSpecialBannerRepository.swift
+//  VariousCollectionViewPattern
+//
+//  Created by 酒井文也 on 2023/04/15.
+//
+
+import Foundation
+
+protocol MainSpecialBannerRepository {}
+
+final class MainSpecialBannerRepositoryImpl: MainSpecialBannerRepository {
+
+    // MARK: - Initializer
+
+    init() {
+        
+    }
+}
