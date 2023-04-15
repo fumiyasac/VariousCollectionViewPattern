e456d85d479ba1c3ad47dfd6f915efbfff9f9f71
fumiyasac <just1factory@gmail.com>
2023-04-15 18:44:55 +0900

    MainFeaturedContentsRepositoryの雛形を新規作成

---
+++ iOSApp/VariousCollectionViewPattern/VariousCollectionViewPattern/Domain/Repository/MainFeaturedContentsRepository.swift
@@ -0,0 +1,19 @@
+//
+//  MainFeaturedContentsRepository.swift
+//  VariousCollectionViewPattern
+//
+//  Created by 酒井文也 on 2023/04/15.
+//
+
+import Foundation
+
+protocol MainFeaturedContentsRepository {}
+
+final class MainFeaturedContentsRepositoryImpl: MainFeaturedContentsRepository {
+
+    // MARK: - Initializer
+
+    init() {
+        
+    }
+}
