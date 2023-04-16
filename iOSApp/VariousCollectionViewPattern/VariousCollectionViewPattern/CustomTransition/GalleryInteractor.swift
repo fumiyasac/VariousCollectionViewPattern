bdfc3e231c55c5c122e2be23cb8b24ea6e9b9772
fumiyasac <just1factory@gmail.com>
2023-04-16 18:29:53 +0900

    フォトギャラリー用Transition関連のクラスを追加

---
+++ iOSApp/VariousCollectionViewPattern/VariousCollectionViewPattern/CustomTransition/GalleryInteractor.swift
@@ -0,0 +1,8 @@
+//
+//  GalleryInteractor.swift
+//  VariousCollectionViewPattern
+//
+//  Created by 酒井文也 on 2023/04/16.
+//
+
+import Foundation
---
+++ iOSApp/VariousCollectionViewPattern/VariousCollectionViewPattern/CustomTransition/GalleryTransition.swift
@@ -0,0 +1,8 @@
+//
+//  GalleryTransition.swift
+//  VariousCollectionViewPattern
+//
+//  Created by 酒井文也 on 2023/04/16.
+//
+
+import Foundation
