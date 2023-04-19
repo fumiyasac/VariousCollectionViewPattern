f2161e2a558b0646ab3b94515ac7c54cf1bf1601
fumiyasac <just1factory@gmail.com>
2023-04-19 22:21:53 +0900

    ContainerView用のViewControllerを追加

---
+++ iOSApp/VariousCollectionViewPattern/VariousCollectionViewPattern/View/Main/HorizontalCarouselMainViewController.swift
@@ -0,0 +1,17 @@
+//
+//  HorizontalCarouselMainViewController.swift
+//  VariousCollectionViewPattern
+//
+//  Created by 酒井文也 on 2023/04/19.
+//
+
+import UIKit
+
+final class HorizontalCarouselMainViewController: UIViewController {
+
+    // MARK: - Override
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ iOSApp/VariousCollectionViewPattern/VariousCollectionViewPattern/View/Main/SnapCarouselMainViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SnapCarouselMainViewController.swift
+//  VariousCollectionViewPattern
+//
+//  Created by 酒井文也 on 2023/04/19.
+//
+
+import UIKit
+
+final class SnapCarouselMainViewController: UIViewController {
+
+    // MARK: - Override
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
