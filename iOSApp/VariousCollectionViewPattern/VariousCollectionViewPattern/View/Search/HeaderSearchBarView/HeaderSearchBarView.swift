f5b8e9ab6face5d5883b655dcddfa2b2035d8997
fumiyasac <just1factory@gmail.com>
2023-04-27 00:01:02 +0900

    HeaderSearchBarViewの要素を追加する

---
+++ iOSApp/VariousCollectionViewPattern/VariousCollectionViewPattern/View/Search/HeaderSearchBarView/HeaderSearchBarView.swift
@@ -0,0 +1,22 @@
+//
+//  HeaderSearchBarView.swift
+//  VariousCollectionViewPattern
+//
+//  Created by 酒井文也 on 2023/04/26.
+//
+
+import Foundation
+import UIKit
+
+final class HeaderSearchBarView: CustomViewBase {
+
+    // MARK: - Initializer
+
+    required init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+}
