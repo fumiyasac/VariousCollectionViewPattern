c985d7263fe3bf50f768c0f1d20dcfbacfe3831b
fumiyasac <just1factory@gmail.com>
2023-05-18 07:30:01 +0900

    ShowcaseDetailViewの新規追加

---
+++ iOSApp/VariousCollectionViewPattern/VariousCollectionViewPattern/View/ShowCase/ShowcaseDetailView.swift
@@ -0,0 +1,20 @@
+//
+//  ShowcaseDetailView.swift
+//  VariousCollectionViewPattern
+//
+//  Created by 酒井文也 on 2023/05/18.
+//
+
+import SwiftUI
+
+struct ShowcaseDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ShowcaseDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        ShowcaseDetailView()
+    }
+}
