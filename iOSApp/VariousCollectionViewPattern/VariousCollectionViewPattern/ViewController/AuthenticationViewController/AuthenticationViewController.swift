aeffb7d7649a64d772cfb3222aa2d0f2f0715ecd
fumiyasac <just1factory@gmail.com>
2023-05-19 11:30:32 +0900

    認証用ViewControllerを新規作成

---
+++ iOSApp/VariousCollectionViewPattern/VariousCollectionViewPattern/ViewController/AuthenticationViewController/AuthenticationViewController.swift
@@ -0,0 +1,17 @@
+//
+//  AuthenticationViewController.swift
+//  VariousCollectionViewPattern
+//
+//  Created by 酒井文也 on 2023/05/19.
+//
+
+import UIKit
+
+final class AuthenticationViewController: UIViewController {
+
+    // MARK: - Navigation
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
