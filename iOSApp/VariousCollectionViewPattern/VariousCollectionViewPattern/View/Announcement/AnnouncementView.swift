59c34eaf7edd92dc4ef25cc32321a45d8c7e393a
fumiyasac <just1factory@gmail.com>
2023-05-20 15:02:15 +0900

    SwiftUI用のViewへ変更

--- iOSApp/VariousCollectionViewPattern/VariousCollectionViewPattern/View/Announcement/AnnouncementView.swift
+++ iOSApp/VariousCollectionViewPattern/VariousCollectionViewPattern/View/Announcement/AnnouncementView.swift
@@ -5,4 +5,16 @@
 //  Created by 酒井文也 on 2023/05/20.
 //
 
-import Foundation
+import SwiftUI
+
+struct AnnouncementView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AnnouncementView_Previews: PreviewProvider {
+    static var previews: some View {
+        AnnouncementView()
+    }
+}
