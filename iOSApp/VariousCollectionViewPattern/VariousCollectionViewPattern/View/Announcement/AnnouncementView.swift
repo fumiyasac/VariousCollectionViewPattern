1723547f5d34fd97ea192ca6fc786460bab389cb
fumiyasac <just1factory@gmail.com>
2023-05-20 15:01:08 +0900

    お知らせ用AnnouncementViewを新規作成

---
+++ iOSApp/VariousCollectionViewPattern/VariousCollectionViewPattern/View/Announcement/AnnouncementView.swift
@@ -0,0 +1,8 @@
+//
+//  AnnouncementView.swift
+//  VariousCollectionViewPattern
+//
+//  Created by 酒井文也 on 2023/05/20.
+//
+
+import Foundation
---
+++ iOSApp/VariousCollectionViewPattern/VariousCollectionViewPattern/ViewModel/AuthenticationViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  AuthenticationViewModel.swift
+//  VariousCollectionViewPattern
+//
+//  Created by 酒井文也 on 2023/05/19.
+//
+
+import Combine
+import Foundation
+
+final class AuthenticationViewModel {
+
+    // MARK: - Initializer
+
+    init() {
+        
+    }
+}
