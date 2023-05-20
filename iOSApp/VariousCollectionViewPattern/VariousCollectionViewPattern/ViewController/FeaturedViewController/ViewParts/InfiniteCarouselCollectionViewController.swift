e20bee9a6be26f337c5e355328d97d5eb91f30dd
fumiyasac <just1factory@gmail.com>
2023-05-21 07:11:08 +0900

    無限Carousel用のタブ画面の新規追加

---
+++ iOSApp/VariousCollectionViewPattern/VariousCollectionViewPattern/ViewController/FeaturedViewController/ViewParts/InfiniteCarouselCollectionViewController.swift
@@ -0,0 +1,17 @@
+//
+//  InfiniteCarouselCollectionViewController.swift
+//  VariousCollectionViewPattern
+//
+//  Created by 酒井文也 on 2023/05/21.
+//
+
+import UIKit
+
+final class InfiniteCarouselCollectionViewController: UIViewController {
+
+    // MARK: - Navigation
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
