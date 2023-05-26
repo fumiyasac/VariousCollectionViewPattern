cbeba1134d83fc566cb5608744bf1d4a98577d11
fumiyasac <just1factory@gmail.com>
2023-05-26 21:47:40 +0900

    久しぶりにStoryboardを追加すつ

---
+++ iOSApp/VariousCollectionViewPattern/VariousCollectionViewPattern/ViewController/FeaturedViewController/ViewParts/CarouselTabView/CarouselTabViewController.swift
@@ -0,0 +1,17 @@
+//
+//  CarouselTabViewController.swift
+//  VariousCollectionViewPattern
+//
+//  Created by 酒井文也 on 2023/05/26.
+//
+
+import UIKit
+
+final class CarouselTabViewController: UIViewController {
+
+    // MARK: - Override
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
