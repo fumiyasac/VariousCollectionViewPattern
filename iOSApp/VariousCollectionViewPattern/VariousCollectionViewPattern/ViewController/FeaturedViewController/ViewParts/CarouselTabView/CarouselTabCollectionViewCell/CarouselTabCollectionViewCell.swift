88c272dd37d9730ae7ac470510233e0fb0d65410
fumiyasac <just1factory@gmail.com>
2023-06-02 17:52:50 +0900

    無限Scroll用のUICollectionViewを新規追加する

---
+++ iOSApp/VariousCollectionViewPattern/VariousCollectionViewPattern/ViewController/FeaturedViewController/ViewParts/CarouselTabView/CarouselTabCollectionViewCell/CarouselTabCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  CarouselTabCollectionViewCell.swift
+//  VariousCollectionViewPattern
+//
+//  Created by 酒井文也 on 2023/06/02.
+//
+
+import UIKit
+
+final class CarouselTabCollectionViewCell: UICollectionViewCell {
+
+    // MARK: - Override
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+}
