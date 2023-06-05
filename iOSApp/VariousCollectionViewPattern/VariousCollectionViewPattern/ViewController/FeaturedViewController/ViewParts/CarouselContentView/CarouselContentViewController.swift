1d9b5eff23d364f5428f9c1f7be7041da8584bea
fumiyasac <just1factory@gmail.com>
2023-06-05 20:44:29 +0900

    無限スクロール用コンテンツ一覧画面の新規追加

---
+++ iOSApp/VariousCollectionViewPattern/VariousCollectionViewPattern/ViewController/FeaturedViewController/ViewParts/CarouselContentView/CarouselContentViewController.swift
@@ -0,0 +1,17 @@
+//
+//  CarouselContentViewController.swift
+//  VariousCollectionViewPattern
+//
+//  Created by 酒井文也 on 2023/06/05.
+//
+
+import UIKit
+
+final class CarouselContentViewController: UIViewController {
+
+    // MARK: - Override
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
