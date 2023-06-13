7cbf6484d382f4b529accf2bf67d90183cc5bea5
fumiyasac <just1factory@gmail.com>
2023-06-13 22:54:38 +0900

    Group整理

---
+++ iOSApp/VariousCollectionViewPattern/VariousCollectionViewPattern/Domain/Repository/AuthenticationRepository.swift
@@ -0,0 +1,8 @@
+//
+//  AuthenticationRepository.swift
+//  VariousCollectionViewPattern
+//
+//  Created by 酒井文也 on 2023/06/13.
+//
+
+import Foundation
--- iOSApp/VariousCollectionViewPattern/VariousCollectionViewPattern/Domain/UseCase/MainUseCase.swift
+++
@@ -1,8 +0,0 @@
-//
-//  MainUseCase.swift
-//  VariousCollectionViewPattern
-//
-//  Created by 酒井文也 on 2023/05/17.
-//
-
-import Foundation
