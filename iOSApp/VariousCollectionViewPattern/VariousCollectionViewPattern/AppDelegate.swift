fd860f71ec9bacd2a423e80bb65902e069b6351f
fumiyasac <just1factory@gmail.com>
2024-03-15 04:45:18 +0900

    ライブラリの整理とTCAの導入

--- iOSApp/VariousCollectionViewPattern/VariousCollectionViewPattern/AppDelegate.swift
+++ iOSApp/VariousCollectionViewPattern/VariousCollectionViewPattern/AppDelegate.swift
@@ -14,18 +14,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
 
-        // MEMO: Dependency Injection用の処理を初期化する
-        if isTesting() {
-            // 注意: Test実行時はDIコンテナへの登録処理をしない
-            // → ●●●Spec内のbeforeEachではテストコードを動作させるのに必要な責務をDIコンテナへ登録
-            // → ●●●Spec内のafterEachではテストコードを動作させるのに登録した責務を削除する
-            print("Build for Unit Testing Starting...")
-        } else {
-            print("Build for Debug/Production Starting...")
-            let productionDependency = DependenciesDefinition()
-            productionDependency.inject()
-        }
-
         // MEMO: iOS15以降で適用するUINavigationController/UITabBarControllerの見た目に関する設定を適用する
         setupNavigationBarAppearance()
         setupTabBarAppearance()
